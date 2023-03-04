818ff53a3a20e7144fc69d1cd2d5889e7230512b
Kilian SMITI <kilian@smiti.fr>
2023-03-04 20:25:20 +0100

    feat: add graven head

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -5,7 +5,8 @@
 <template>
 <div class="message">
   <div class="message__content">
-    Hello, world !
+    <img class="message__avatar" src="/images/graven.jpg" alt="it's graven !" width="269" height="269" />
+    <p class="message__text">Bonjour moi c'est Graven !</p>
   </div>
 </div>
 </template>
@@ -15,9 +16,22 @@
   padding: var(--spacing-medium);
 
   &__content {
+    display: flex;
+    align-items: flex-start;
     max-width: var(--conversation-size);
     margin: auto;
     padding: var(--spacing-medium);
+  }
+
+  &__avatar {
+    width: 30px;
+    height: 30px;
+    margin: var(--spacing-little);
+  }
+
+  &__text {
+    margin: var(--spacing-little);
+    font-size: 1.5rem;
   }
 }
 </style>
